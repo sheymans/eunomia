e202ff7b986b3955463b25b80c434f621647d9e6
Stijn Heymans <stijn.heymans@gmail.com>
2014-12-04 16:22:01 -0800

    Getting values in AtomIndex

--- eunomia/index.py
+++ eunomia/index.py
@@ -12,6 +12,13 @@ class AtomIndex(object):
             self.index[pred] = {}
         self.__add_args(self.index[pred], atom.args, value)
 
+    def get_values(self, atom):
+        pred = atom.predicate.value
+        if pred not in self.index:
+            return None
+        else:
+            return self.__find(self.index[pred], atom.args)
+
     ## Private functions
     def __add_args(self, dic, args, value):
         if len(args) > 1:
@@ -35,6 +42,21 @@ class AtomIndex(object):
                 dic[key_val] = [value]
             elif value not in dic[key_val]:
                 dic[key_val].append(value) 
+
+    def __find(self, dic, args):
+        if len(args) > 0:
+            el = args[0]
+            if el.is_var:
+                key_val = -1
+            else:
+                key_val = el.value
+
+            if key_val not in dic:
+                return None
+            else:
+                return self.__find(dic[key_val], args[1:])
+        else:
+            return dic
 
     ## Built-ins
     def __str__(self):
--- tests/test_index.py
+++ tests/test_index.py
@@ -21,3 +21,23 @@ class TestIndex(unittest.TestCase):
         ind.add(at3, value)
         self.assertEqual(ind.index, {'p': {'a': {'d': ['random'], -1: ['random', 'random2']}}})
 
+    def test_get_values(self):
+        ind = AtomIndex()
+        at = Atom(Term("p"), [Term("a"), Term("?x", True)])
+        at2 = Atom(Term("p"), [Term("a"), Term("?y", True)])
+        value = "random"
+        value2 = "random2"
+
+        ind.add(at, value)
+        ind.add(at, value2)
+        ind.add(at2, value2)
+
+        self.assertEqual(ind.get_values(at), ['random', 'random2'])
+        self.assertEqual(ind.get_values(at2), ['random', 'random2'])
+
+        at3 = Atom(Term("p"), [Term("a"), Term("d")])
+        ind.add(at3, value)
+        self.assertEqual(ind.get_values(at3), ['random' ])
+
+
+
